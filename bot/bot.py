45d2b36ae40023cf36d4c151d04e74a838052c55
Ubaldo Puocci <u.puocci@gmail.com>
2017-07-11 21:45:52 +0200

    bot version 1.1 & added broadcast

--- bot/bot.py
+++ bot/bot.py
@@ -3,69 +3,61 @@ import sys
 from flask import Flask, request
 import telepot
 import urllib3
-from datetime import datetime, timedelta
-import calendar
 import pickle
 import unicodedata
+import calendar
+from datetime import datetime, timedelta
+import bot_parser
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 replies = {
     'help': 'Questo bot da informazioni sul calendario della pulizia delle strade '
     'nei comuni di Firenze e Sesto Fiorentino. Per avere informazioni su una strada '
-    'mandare semplicemente il nome della stessa a questo bot. Se il bot si trova in '
-    'una chat, il messaggio deve contenere come prima parola "@LuckyCloverBot".',
+    'mandare semplicemente il nome della stessa a questo bot.',
 }
-my_id = 999999
 proxy_url = "http://proxy.server:3128"
-KEY = '297564683:XXXXXXXXXXXXXXXXX'
+KEY = '297564683:AAHq3cetrsnUehJO1zFqMIAgVYNg9gVh5Lg'
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
 }
 telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-secret = "XXXXXXXXXXXXXXXXXXXXX"
+secret = "42"
 bot = telepot.Bot(KEY)
 bot.setWebhook("https://eyedema.pythonanywhere.com/{}".format(secret), max_connections=1)
-bot.sendMessage(my_id, "✅  Bot started.")
+bot.sendMessage(23616716, "✅  Bot started.")
 app = Flask(__name__)
 
+
+
 def load_obj(name):
-    with open('/home/Eyedema/obj/' + name + '.pkl', 'rb') as f:
+    with open('/home/Eyedema/luckycloverbot/obj/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 listaStradeFi = load_obj('listaFi')
 listaStradeSf = load_obj('listaSf')
 
-def write_log(text, name, userID):
-    with open('/home/Eyedema/obj/botlog.log', 'a+') as file:
-        file.write(':: {:%Y-%b-%d %H:%M:%S} '.format(datetime.now())+name+' (ID:'+str(userID)+') searched for '+text+'\n')
+def write_log(update):
+    username = "---"
+    try:
+        username = update["message"]["from"]["username"]
+    except Exception:
+        pass
+    text, chat_id, name = bot_parser.parse_update(update)
+    with open('/home/Eyedema/luckycloverbot/obj/botlog.log', 'a+') as file:
+        file.write(':: {:%Y-%b-%d %H:%M:%S} '.format(datetime.now())+name+' @'+username+' (ID:'+str(chat_id)+') searched for '+text+'\n')
+    bot_parser.add_to_broadcast(update)
 
-def build_string(suffix):
-    suffix = suffix.upper()
-    appendix = suffix.rsplit(None, 1)[-1]
-    return appendix.split("'")[-1]
-
-def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])+"'"
-
-def hours_alert(date):
-    d1 = datetime.strptime(date, '%Y-%m-%d')
-    d2 = d1 - timedelta(days=1)
-    hours = (datetime.now().hour) - 7
-    return abs((d2 - datetime.today()).days*24 - hours)
-
-def get_schedule(dayN, nthDay):
-    this_month_schedule = calendar.Calendar(dayN).monthdatescalendar(datetime.today().year, datetime.today().month)[nthDay][0]
-    next_month_schedule = calendar.Calendar(dayN).monthdatescalendar(datetime.today().year, datetime.today().month+1)[nthDay][0]
-    return str(this_month_schedule) if this_month_schedule > datetime.today().date() else str(next_month_schedule)
-
-def send_message(chat_id, dayN, nthDay):
-    schedule = get_schedule(dayN, nthDay)
-    alert = hours_alert(schedule)
+def send_schedule(update, dayN, nthDay):
+    chat_id = bot_parser.parse_update(update)[1]
+    schedule = bot_parser.get_schedule(dayN, nthDay)
+    alert = bot_parser.hours_alert(schedule)
     bot.sendMessage(chat_id, "{} {}\nOrario 00.00 - 06.00".format(calendar.day_name[dayN], schedule))
     bot.sendMessage(chat_id, "/alert "+str(alert)+"h Spostare la macchina.")
+    write_log(update)
+
 
 def check_florence(chat_id, msg, strada):
     testoMessaggio = "Firenze:\n\n"
@@ -87,49 +79,60 @@ def check_sesto(chat_id, msg, strada):
         return 1
     return 0
 
-def parse_message(chat_id, update):
-    text = update["message"]["text"]
+def parse_message(update):
+    text, chat_id, name = bot_parser.parse_update(update)
     msg = 0
     try:
         text.decode('ascii')
     except (UnicodeEncodeError, UnicodeDecodeError), e:
-        stradaX = build_string(text)
-        strada = remove_accents(stradaX)
+        stradaX = bot_parser.build_string(text)
+        strada = bot_parser.remove_accents(stradaX)
     else:
-        strada = build_string(text)
+        strada = bot_parser.build_string(text)
     msg += check_florence(chat_id, msg, strada)
     msg += check_sesto(chat_id, msg, strada)
     if msg == 0:
         bot.sendMessage(chat_id, "Strada non trovata")
-    write_log(update["message"]["text"], update["message"]["from"]["first_name"], chat_id)
+    write_log(update)
 
-def welcome(chat_id):
-    bot.sendMessage(chat_id,replies['help'])
+def welcome(update):
+    bot.sendMessage(bot_parser.parse_update(update)[1],replies['help'])
+    write_log(update)
+
 
 def send_info(update):
-    message = "Chat ID: "+str(update["message"]["chat"]["id"])+"\nChat type: "+update["message"]["chat"]["type"]
-    bot.sendMessage(update["message"]["chat"]["id"], message)
-    write_log(update["message"]["text"], update["message"]["from"]["first_name"],update["message"]["chat"]["id"])
+    message = "Chat ID: "+str(bot_parser.parse_update(update)[1])+"\nChat type: "+update["message"]["chat"]["type"]
+    bot.sendMessage(bot_parser.parse_update(update)[1], message)
+    write_log(update)
+
+def send_message(update):
+    text = bot_parser.parse_update(update)[0]
+    if len(text.split())>2:
+        bot.sendMessage(text.split()[1], "".join(text.split()[2:]))
 
 @app.route('/{}'.format(secret), methods=["POST"])
 def telegram_webhook():
     update = request.get_json()
     if "message" in update:
         try:
-            text = update["message"]["text"]
-            chat_id = update["message"]["chat"]["id"]
+            text = bot_parser.parse_update(update)[0]
+            from_id = update["message"]["from"]["id"]
             if text == '/greco' or text == '/greco@LuckyCloverBot':
-                send_message(chat_id, 4,1)
+                send_schedule(update, 4,1)
             elif text == '/kyoto' or text == '/kyoto@LuckyCloverBot':
-                send_message(chat_id, 6,2)
+                send_schedule(update, 6,2)
             elif text == '/start':
-                welcome(chat_id)
-            elif text == '/getinfo' and update["message"]["from"]["id"] == my_id:
+                welcome(update)
+            elif text == '/getinfo' and  from_id== 23616716:
                 send_info(update)
             elif text.split()[0] == '@LuckyCloverBot':
-                parse_message(chat_id, update)
+                parse_message(update)
+            elif text == '/getlist' and from_id == 23616716:
+                bot.sendMessage(23616716, bot_parser.get_broadcast())
+            elif text.split()[0] == '/send' and from_id == 23616716:
+                send_message(update)
             elif update["message"]["chat"]["type"] == 'private':
-                parse_message(chat_id, update)
+                parse_message(update)
         except KeyError:
             pass
     return "OK"
---
+++ bot/bot_parser.py
@@ -0,0 +1,62 @@
+# encoding=utf8
+import sys
+from datetime import datetime, timedelta
+import calendar
+import unicodedata
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+def build_string(suffix):
+    suffix = suffix.upper()
+    appendix = suffix.rsplit(None, 1)[-1]
+    return appendix.split("'")[-1]
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])+"'"
+
+def hours_alert(date):
+    d1 = datetime.strptime(date, '%Y-%m-%d')
+    d2 = d1 - timedelta(days=1)
+    hours = (datetime.now().hour) - 7
+    return abs((d2 - datetime.today()).days*24 - hours)
+
+def get_schedule(dayN, nthDay):
+    this_month_schedule = calendar.Calendar(dayN).monthdatescalendar(datetime.today().year, datetime.today().month)[nthDay][0]
+    next_month_schedule = calendar.Calendar(dayN).monthdatescalendar(datetime.today().year, datetime.today().month+1)[nthDay][0]
+    return str(this_month_schedule) if this_month_schedule > datetime.today().date() else str(next_month_schedule)
+
+def parse_update(update):
+    return update["message"]["text"], update["message"]["chat"]["id"], update["message"]["from"]["first_name"]
+    #testo, id, nome
+
+def write_broadcast(text):
+    if text not in open("/home/Eyedema/luckycloverbot/broadcast").read():
+        with open("/home/Eyedema/luckycloverbot/broadcast", "a") as myfile:
+            myfile.write(text)
+
+def add_to_broadcast(update):
+    username = "---"
+    try:
+        username = update["message"]["from"]["username"]
+    except Exception:
+        pass
+    text, chat_id, name = parse_update(update)
+    message = ""
+    if update["message"]["chat"]["type"] == 'private':
+        message += str(chat_id) +' '+ name +' @'+ username +'\n'
+        write_broadcast(message)
+    else:
+        name = update["message"]["chat"]["title"]
+        username = '---'
+        message += str(chat_id) +' '+ name +' @'+ username +'\n'
+        write_broadcast(message)
+
+def get_broadcast():
+    message = ""
+    with open("/home/Eyedema/luckycloverbot/broadcast", 'r') as myfile:
+        message = myfile.read()
+    if message:
+        return message
+    return "Broadcast list empty."

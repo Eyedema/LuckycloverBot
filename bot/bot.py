b96691b7476d2fc4c96d3a2d902065d6e085b666
Ubaldo <u.puocci@gmail.com>
2017-07-11 22:17:43 +0200

    updated

--- bot/bot.py
+++ bot/bot.py
@@ -19,14 +19,13 @@ replies = {
     'mandare semplicemente il nome della stessa a questo bot.',
 }
 proxy_url = "http://proxy.server:3128"
-KEY = 'MY_KEY'
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
 }
 telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
 secret = "42"
-bot = telepot.Bot(KEY)
+bot = telepot.Bot(MY_KEY)
 bot.setWebhook("https://eyedema.pythonanywhere.com/{}".format(secret), max_connections=1)
 bot.sendMessage(MY_CHAT, "✅  Bot started.")
 app = Flask(__name__)
